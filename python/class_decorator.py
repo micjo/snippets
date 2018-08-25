f2563647e51718faedd1b1b1fac1bfedad87129b
micjo <>
2018-08-25 16:57:29 +0200

    Changes the class_decorator into a timer

--- python/class_decorator.py
+++ python/class_decorator.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
-class DecoratorClass(object):
+class Timer(object):
+
     def __init__(self, orig_func):
         self.orig_func = orig_func
 
     def __call__(self, *args, **kwargs):
-        print " >> wrapper Function"
-        return self.orig_func(*args, **kwargs)
+        # This means importing time has to happen each time you call the function.
+        # Seems to me this is done more elegantly in the decorators without classes.
+        import time
+        start_time = time.time()
+        result = self.orig_func(*args, **kwargs)
+        end_time = time.time()
+        delta_time = end_time - start_time
+        print "{} ran for {} seconds".format(self.orig_func.__name__, delta_time)
+        return result
 
 
-@DecoratorClass
+@Timer
 def display(name, age):
     print "display called with args ({}, {})".format(name,age)
 
