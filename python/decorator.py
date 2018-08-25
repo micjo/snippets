2861000554a568c2c68ee48d7a8b8d8cffc3dfde
micjo <>
2018-08-25 16:31:19 +0200

    Added wraps decorator to the wrappers. This makes sure that when you get attributes from the decorated function,such as __name__, it is as if you are referring to the original function

--- python/decorator.py
+++ python/decorator.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
+from functools import wraps
+
 def time_measure(orig_func):
     import time
+
+    @wraps(orig_func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = orig_func(*args, **kwargs)
@@ -14,6 +18,8 @@ def time_measure(orig_func):
 def log_call_to_file(orig_func):
     import logging
     logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    @wraps(orig_func)
     def wrapper(*args, **kwargs):
         logging.info("Function <{}> ran with args:{} and kwargs:{}"
                 .format(orig_func.__name__,args,kwargs))
