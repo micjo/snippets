cb681ea6853c37d8d23f1d726c53c9250463d1f9
micjo <>
2018-08-25 15:55:43 +0200

    Removed extra logging

--- python/decorator.py
+++ python/decorator.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python
 
 def time_decorator(orig_function):
-    print "> start: time_decorator"
     def wrapper():
-        print ">> start: wrapper"
         result = orig_function()
-        print ">> end: wrapper"
-    print "> end: time_decorator"
     return wrapper
 
 
-print "start: Decorating display function"
 @time_decorator
 def display():
     print ">>> This is a simple display function"
-print "end: Decorating display function"
-
 
 display()
