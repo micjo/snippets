d8dbbb95f2c0642f0885ead47f2f39e4e2adeb0f
micjo <>
2018-08-25 18:04:40 +0200

    Implemented timer

--- python/decorating_entire_class.py
+++ python/decorating_entire_class.py
@@ -2,9 +2,14 @@
 
 
 def time_this(orig_func):
-    print "timing"
+    import time
     def wrapper():
-        return orig_func()
+        start_time = time.time()
+        result = orig_func()
+        end_time = time.time()
+        delta_time = end_time - start_time
+        print "{} ran in {} seconds".format(orig_func.__name__, delta_time)
+        return result
     return wrapper
 
 def time_all_class_methods(Decoratee):
@@ -13,8 +18,8 @@ def time_all_class_methods(Decoratee):
             self.oInstance = Decoratee()
 
         def __getattribute__(self,s):
-
-            #check if attribute exists in non-overwritten function
+            #check if attribute exists in non-overwritten function and return this one
+            #If you dont do this you will be stuck in infinite recursion
             try:
                 x = super(Decorator,self).__getattribute__(s)
             except AttributeError:
